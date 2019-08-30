52390b7602bcb73c8144f741c061bbe938865d75
dora <31735614+dora1998@users.noreply.github.com>
2019-08-31 00:45:30 +0900

    Implement CR in CRUD

--- main.rb
+++ main.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
+require 'json'
+
+posts = [{title: 'aaaa', body: 'bbb'}]
 
 get '/' do
   'Hello world!'
+end
+
+get '/posts' do
+  posts.inject('') { |result, p| result += "#{p[:title]}: #{p[:body]}<br>" }
+end
+
+post '/posts' do
+  data = JSON.parse request.body.read
+  puts data
+  posts.append(title: data["title"], body: data["body"])
+  "#{data["title"]}: #{data["body"]}"
+end
+
+get '/posts/:id' do
+  'Not found' if params[:id].positive? || params[:id] >= posts.length
+  "#{p[:title]}: #{p[:body]}<br>"
 end
